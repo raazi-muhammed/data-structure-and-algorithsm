724b7166155f7c5bb1842bf818849c1861701182
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-11-18 11:40:46 +0530

    Added recursive solutions to binary search

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -13,4 +13,22 @@ const binarySearch = (arr, target) => {
 	return -1;
 }; // o(logn)T
 
-module.exports = { binarySearch };
+const binarySearchRecursive = (arr, target) => {
+	let leftIndex = 0;
+	let rightIndex = arr.length - 1;
+
+	return recursiveCall(arr, target, leftIndex, rightIndex);
+};
+
+const recursiveCall = (arr, target, leftIndex, rightIndex) => {
+	if (leftIndex > rightIndex) return -1;
+	let middleItemIndex = Math.floor((leftIndex + rightIndex) / 2);
+	if (target === arr[middleItemIndex]) return middleItemIndex;
+
+	if (target < arr[middleItemIndex])
+		return recursiveCall(arr, target, leftIndex, middleItemIndex - 1);
+	if (target > arr[middleItemIndex])
+		return recursiveCall(arr, target, middleItemIndex + 1, rightIndex);
+}; // o(logn)T
+
+module.exports = { binarySearch: binarySearchRecursive };
