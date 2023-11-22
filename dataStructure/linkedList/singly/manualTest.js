523c2e5282f5b2d20df25bd0abebbccac05207d8
Raazi Muhammed K <raazi6163@gmail.com>
2023-11-23 00:09:24 +0530

    Delete dataStructure/linkedList/singly/manualTest.js

--- dataStructure/linkedList/singly/manualTest.js
+++
@@ -1,7 +0,0 @@
-const findFibonacciRecursive = (index) => {
-	if (index <= 1) return index;
-	return findFibonacciRecursive(index - 1) + findFibonacciRecursive(index - 2);
-}; // O(2^n)ST
-
-let a = findFibonacciRecursive(5);
-console.log(a);
