b33511db80d8c6120dc3d94900b832c5809f7447
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-11-26 09:27:56 +0530

    Added merge and quick sort

--- algorithms/sorting/sorting.test.js
+++ algorithms/sorting/sorting.test.js
@@ -1,10 +1,12 @@
 const { bubbleSort } = require("./bubbleSort");
 const { insertionSort } = require("./insertionsSort");
+const { mergeSort } = require("./mergeSort");
+const { quickSort } = require("./quickSort");
 const { selectionSort } = require("./selectionSort");
 
 test("Testing sort", () => {
-  expect(selectionSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
-  expect(selectionSort([4, 2, 3, 5])).toStrictEqual([2, 3, 4, 5]);
-  expect(selectionSort([4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4]);
-  expect(selectionSort([40, -2, -30, 1])).toStrictEqual([-30, -2, 1, 40]);
+  expect(mergeSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
+  expect(mergeSort([4, 2, 3, 5])).toStrictEqual([2, 3, 4, 5]);
+  expect(mergeSort([4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4]);
+  expect(mergeSort([40, -2, -30, 1])).toStrictEqual([-30, -2, 1, 40]);
 });
