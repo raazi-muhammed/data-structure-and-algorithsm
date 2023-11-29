2c9ddde057e91f715bd78d668a1b41bdcbebc301
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-11-29 11:55:43 +0530

    Added js doc

--- dataStructure/linkedList/stackAndQueue/stack.js
+++ dataStructure/linkedList/stackAndQueue/stack.js
@@ -1,57 +1,71 @@
+//@ts-check
+
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.down = null;
-  }
+    /** @type {number} */
+    value;
+    /** @type {Node | null} */
+    down;
+
+    /** @param {number} value */
+    constructor(value) {
+        this.value = value;
+        this.down = null;
+    }
 }
 
 class Stack {
-  top = null;
-  size = 0;
-  limit = null;
+    /** @type {Node | null} */
+    top = null;
+    size = 0;
+    /** @type {number | null} */
+    limit = null;
 
-  constructor(values = []) {
-    if (values.length <= 0) {
-      this.top = null;
-      this.size = 0;
-    } else {
-      values.map((val) => {
-        this.push(val);
-      });
-    }
-  }
-
-  push(value) {
-    if (this.limit != null && this.size >= this.limit) return;
-
-    const node = new Node(value);
-
-    if (this.size > 0) node.down = this.top;
-    this.top = node;
-
-    this.size++;
-  }
-
-  pop() {
-    if (this.size <= 0) return;
-    this.top = this.top.down;
-    this.size--;
-  }
-
-  setLimit(val) {
-    this.limit = val;
-  }
-
-  getValues() {
-    let array = [];
-    let current = this.top;
-    while (current) {
-      array.push(current.value);
-      current = current.down;
+    /** @param {number[]} [values] */
+    constructor(values = []) {
+        if (values.length <= 0) {
+            this.top = null;
+            this.size = 0;
+        } else {
+            values.map((val) => {
+                this.push(val);
+            });
+        }
     }
 
-    return array;
-  }
+    /** @param {number} value */
+    push(value) {
+        if (this.limit != null && this.size >= this.limit) return;
+
+        const node = new Node(value);
+
+        if (this.size > 0) node.down = this.top;
+        this.top = node;
+
+        this.size++;
+    }
+
+    pop() {
+        if (this.top == null) return;
+        this.top = this.top.down;
+        this.size--;
+    }
+
+    /** @param {number} val */
+    setLimit(val) {
+        this.limit = val;
+    }
+
+    /** @returns {number[]} */
+    getValues() {
+        let array = [];
+        let current = this.top;
+        while (current) {
+            array.push(current.value);
+            current = current.down;
+        }
+
+        return array;
+    }
 }
 
 module.exports = { Stack };
