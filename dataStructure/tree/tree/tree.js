69c4b949c4dcd00db67bc9c844f523cc502977d5
Raazi Muhammed <70052135+raazi-muhammed@users.noreply.github.com>
2023-12-08 19:50:11 +0530

    Added tree

--- dataStructure/tree/tree/tree.js
+++ dataStructure/tree/tree/tree.js
@@ -2,23 +2,13 @@ class Node {
     /** @type {number} */
     value;
 
-    /** @param {Map} */
+    /** @type {Array} */
     children;
 
     /** @param {number} value  */
     constructor(value) {
         this.value = value;
-        this.children = new Map();
-    }
-
-    /** @param {Node} value  */
-    addChild(value) {
-        this.children.set(value, value);
-    }
-
-    /** @param {Node} value  */
-    removeChild(value) {
-        this.children.delete(value);
+        this.children = [];
     }
 }
 
@@ -26,10 +16,67 @@ class Tree {
     /** @type {Node} */
     root;
 
-    constructor(rootVal) {
-        const node = new Node(rootVal);
-        this.root = node;
+    /** @type {number} */
+    size;
+
+    constructor() {
+        this.root = null;
+        this.size = 0;
     }
 
-    addChild(parent) {}
+    insert(value) {
+        const node = new Node(value);
+        if (this.root === null) this.root = node;
+        else {
+            let isInserted = false;
+            /** @type {Array} */
+            let children = this.root.children;
+
+            for (let i = 0; i < 3; i++) {
+                while (!isInserted) {
+                    if (children.length < 3) {
+                        children.push(node);
+                        isInserted = true;
+                    } else {
+                        children = children[i].children;
+                    }
+                }
+                if (isInserted) break;
+            }
+        }
+        this.size++;
+    }
+
+    hasValue(target) {
+        let current = this.root;
+        while (current) {
+            if (current.value === target) return true;
+            for (let i = 0; i < current.children.length; i++) {
+                if (current.children[i].value === target) return true;
+            }
+            current = current.children[0];
+        }
+        return false;
+    }
+
+    getValues() {
+        let current = this.root;
+        let array = new Set();
+
+        while (current) {
+            array.add(current.value);
+            for (let i = 0; i < current.children.length; i++) {
+                array.add(current.children[i].value);
+            }
+
+            current = current.children[0];
+        }
+        return [...array];
+    }
+
+    getSize() {
+        return this.size;
+    }
 }
+
+module.exports = { Tree };
--- dataStructure/tree/tree/tree.test.js
+++ dataStructure/tree/tree/tree.test.js
@@ -1,1 +1,39 @@
-describe("Tree", () => {});
+const { Tree } = require("./tree");
+
+describe("Tree", () => {
+    test("Insert", () => {
+        const tree = new Tree();
+
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(10);
+
+        expect(tree.getValues()).toEqual([1, 6, 10]);
+    });
+    test("Search", () => {
+        const tree = new Tree();
+
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(10);
+
+        expect(tree.hasValue(1)).toBe(true);
+        expect(tree.hasValue(12)).toBe(false);
+        expect(tree.hasValue(10)).toBe(true);
+    });
+    test("Get size and values from tree", () => {
+        const tree = new Tree();
+
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(10);
+
+        expect(tree.getSize(1)).toBe(3);
+        expect(tree.getValues()).toEqual([1, 6, 10]);
+
+        tree.insert(11);
+        tree.insert(13);
+        expect(tree.getSize(1)).toBe(5);
+        expect(tree.getValues()).toEqual([1, 6, 10, 11, 13]);
+    });
+});
